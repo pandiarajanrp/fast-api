69bbb5fdc05abdbdba431b2319e63d7bec6ff917
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-07-19 15:19:52 -0600

    path param
    path param

--- books.py
+++ books.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Get Req
 @app.get("/books")
 async def get_all_books():
   return {"message": "Hello World!!!###"}
+
+#Path Param
+@app.get("/books/{book_id}")
+async def get_all_books(book_id):
+  return {"message": f"Getting details of Book with ID {book_id}"}
