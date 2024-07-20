6675ec2b724ab8be8dbdae66b2dd344e7c5622c7
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-07-20 12:10:03 -0600

    post routes

--- model/post.py
+++ model/post.py
@@ -22,9 +22,20 @@ class PostRequest(BaseModel):
   author: str = Field(min_length=4)
   rating: float = Field(gt=0, lt=6)
 
+  model_config = {
+    "json_schema_extra": {
+      "example": {
+        "title": "Basics of Js",
+        "content": "Learn the basics of JS",
+        "author": "RP",
+        "rating": 5
+      }
+    }
+  }
+
 POSTS = [
   Posts(1, "Importance of Basic", "Importance of basics in Software Development", "PR", 4),
-  Posts(1, "Kubernetes Basics", "Kubernetes Basics in containersation", "PR", 3.5),
-  Posts(1, "Python Advanced", "Python Advanced in Software Development", "PR", 3.6)
+  Posts(2, "Kubernetes Basics", "Kubernetes Basics in containersation", "PR", 3.5),
+  Posts(3, "Python Advanced", "Python Advanced in Software Development", "PR", 3.6)
 ]
 
--- post.py
+++ post.py
@@ -9,8 +9,30 @@ async def get_posts():
 
 @app.post("/posts")
 async def create_post(payload: PostRequest):
+  #old approach with Body
   #POSTS.append(Posts(**payload["id"], payload["title"], payload["content"], payload["author"], payload["rating"]))
   #POSTS.append(Posts(**payload.dict()))
   new_book = Posts(**payload.dict())
-  POSTS.append(new_book)
+  POSTS.append(get_post_id(new_book))
   return POSTS
+
+@app.get("/posts/{id}")
+async def get_post_by_id(id: int):
+  return next(filter(lambda el: el.id == id, POSTS), None)
+
+@app.put("/posts/{id}")
+async def update_post_by_id(id: int, payload: PostRequest):
+  post = next(filter(lambda el: el.id == id, POSTS), None)
+  post = payload
+  post.id = id
+  print(post)
+  return POSTS
+
+@app.delete("/posts/{id}")
+async def delete_post_by_id(id: int):
+  result = next((item for item in list(POSTS) if item["id"] == id), None)
+  print(result)
+
+def get_post_id(post):
+  post.id = 1 if len(POSTS) == 0 else POSTS[-1].id + 1
+  return post
