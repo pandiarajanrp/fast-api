0ef13dd17408752e4535ab593e7e956f066a89f8
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-07-24 19:42:13 -0600

    secure route added

--- todo-app/routers/users.py
+++ todo-app/routers/users.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from utils import db_dependency
 from models import Users
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from typing import Annotated
-from jose import jwt
+from jose import jwt, JWTError
 from datetime import datetime,timezone, timedelta
 
 SECRET_KEY="5bc0bbad167b20965aef2dd72546e9e1534c97d5704d258fbec1b54a1f1fee9a"
 ALGORITHM="HS256"
 
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="users/auth")
 class CreateUserRequest(BaseModel):
   username: str
   email: str
@@ -17,6 +18,10 @@ class CreateUserRequest(BaseModel):
   last_name: str
   password: str
   role: str
+
+class AuthResponse(BaseModel):
+  token: str
+  token_type: str
 
 router = APIRouter(
   prefix="/users",
@@ -30,12 +35,26 @@ def authenticate_user(username, password, db):
   else:
     return user
   
+def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+  try:
+    user = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    username = user.get('sub')
+    user_id = user.get('id')
+    if username is None or user_id is None:
+      raise HTTPException(status_code=401, detail="Unauthorized")
+    return {
+      'username': username,
+      'user_id': user_id
+    }
+  except JWTError:
+    raise HTTPException(status_code=401, detail="Unauthorized")
+  
 def create_access_token(username, user_id, exp):
   expires = datetime.now(timezone.utc) + exp
   encode = { 'sub': username, 'id': user_id, 'exp': expires }
   return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
-
+auth_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.post("/")
 async def create_user(db: db_dependency, payload: CreateUserRequest):
@@ -44,14 +63,17 @@ async def create_user(db: db_dependency, payload: CreateUserRequest):
   db.commit()
   return user_model
 
-@router.post("/auth")
+@router.post("/auth", response_model=AuthResponse)
 async def get_auth_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: db_dependency
   ):
   user = authenticate_user(form_data.username, form_data.password, db)
+  if not user:
+    raise HTTPException(status_code=400, detail="Invalid user")
   token = create_access_token(user.username, user.id, timedelta(minutes=20))
   
   return {
-    'token': token
+    'token': token,
+    'token_type': 'password'
   }
